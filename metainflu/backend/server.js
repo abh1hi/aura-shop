ffa7c28e927f92cdceb10d379cca8e9002854d29
abh1hi <abhinavbhatnaria5@gmail.com>
2025-10-18 02:49:12 +0530

    removed vendor-aoo

--- metainflu/backend/server.js
+++ metainflu/backend/server.js
@@ -28,7 +28,12 @@ const mongoURI = process.env.MONGO_URI;
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+'http://localhost:5174',
+'http://127.0.0.1:5174'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
