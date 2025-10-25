6187c7bb2063032903af59e9ac2d36ae8cb099d4
abh1hi <abhinavbhatnaria5@gmail.com>
2025-10-25 18:30:08 +0530

    removed new vendor ui

--- metainflu/adminpanel/frontend/vendor-new-ui/src/services/vendorApi.js
+++
@@ -1,5 +0,0 @@
-export const vendorApi = {
-  async programs(){ throw new Error('mock') },
-  async payoutsSummary(){ throw new Error('mock') },
-  async notifications(){ throw new Error('mock') },
-}
